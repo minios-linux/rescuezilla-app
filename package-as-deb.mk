156c1d5da32ff5bd828fa67983b9e4d0f8bb77ac
crims0n <crims0n@minios.dev>
2025-07-11 02:00:05 +0300

    Refactor version handling in Makefile to use changelog for version string and simplify tarball creation

--- package-as-deb.mk
+++ package-as-deb.mk
@@ -11,23 +11,11 @@ APP_NAME?=NO_APP_NAME_SET
 # TODO: to check the exit codes of the below shell commands, so improve resilience of this Makefile.
 # [1] https://stackoverflow.com/a/40710111
 
-# Get the most recent git tag of a versioned release (deb files don't support git SHA's, so need to abbreviate)
-#
-# Note: the --match is a glob, not a regex.
-LAST_TAGGED_VERSION=$(shell git describe --tags  --match="[0-9].[0-9]*" --abbrev=0)
-# Full git version to embed
-# If the current commit is not tagged, the version number from `git
-# describe --tags  --match="[0-9].[0-9].[0-9]" --dirty` is X.Y.Z-abc-gGITSHA-dirty,
-# where X.Y.Z is the previous versioned tag, 'abc' is the number of commits since that tag, gGITSHA is the git sha
-# prepended by a 'g', and -dirty is present if the working tree has been modified.
-#
-# Note: the --match is a glob, not a regex.
-VERSION_STRING=$(shell git describe --tags --match="[0-9].[0-9]*" --dirty)
-# Date of current git commit in colon-less ISO 8601 format (2013-04-01T130102)
-GIT_COMMIT_DATE=$(shell date +"%Y-%m-%dT%H%M%S" --date=@$(shell git show --no-patch --format=%ct HEAD))
+# Get the version from the first line of debian/changelog (without suffix after first dash)
+VERSION_STRING=$(shell head -n1 ./debian/changelog | sed -n 's/^[^ ]* (\([^)-]*\).*/\1/p')
 
 WORKING_DIR=$(abspath $(DEB_BUILD_DIR)/build_script_modified_source)
-PACKING_DIR=$(abspath $(DEB_BUILD_DIR)/$(APP_NAME)-$(LAST_TAGGED_VERSION))
+PACKING_DIR=$(abspath $(DEB_BUILD_DIR)/$(APP_NAME)-$(VERSION_STRING))
 
 all:	embed-version deb
 
@@ -51,14 +39,15 @@ embed-version: copy
 deb:
 	mkdir --parents $(PACKING_DIR) || true
 	$(info * Create gzipped tar archive of Rescuezilla frontend app source code.)
-	cd $(WORKING_DIR)/$(APP_NAME) && tar -czf $(ABS_BUILD_PATH)/$(APP_NAME)_$(LAST_TAGGED_VERSION).orig.tar.gz ./
+	cd $(WORKING_DIR)/$(APP_NAME) && tar -czf $(ABS_BUILD_PATH)/$(APP_NAME)_$(VERSION_STRING).orig.tar.gz ./
 	$(info * Create gzipped tar archive of debian-specific configuration directory.)
-	cd $(WORKING_DIR)/ && tar -czf $(ABS_BUILD_PATH)/$(APP_NAME)_$(LAST_TAGGED_VERSION).debian.tar.gz ./debian/
+	cd $(WORKING_DIR)/ && tar -czf $(ABS_BUILD_PATH)/$(APP_NAME)_$(VERSION_STRING).debian.tar.gz ./debian/
 	$(info * Extract $(APP_NAME) source tarball. This makes a copy of the source tree in the build directory)
-	cd $(PACKING_DIR) && tar -xzf $(ABS_BUILD_PATH)/$(APP_NAME)_$(LAST_TAGGED_VERSION).orig.tar.gz 
+	cd $(PACKING_DIR) && tar -xzf $(ABS_BUILD_PATH)/$(APP_NAME)_$(VERSION_STRING).orig.tar.gz 
 	# Make a copy of the source debian-specific configuration in the build directory
 	$(info * Extract debian-specific configuration tarball. This makes a copy of the debian-specific configuration in the build directory)
-	cd $(PACKING_DIR) && tar -xzf $(ABS_BUILD_PATH)/$(APP_NAME)_$(LAST_TAGGED_VERSION).debian.tar.gz 
+	cd $(PACKING_DIR) && tar -xzf $(ABS_BUILD_PATH)/$(APP_NAME)_$(VERSION_STRING).debian.tar.gz
+	cd $(PACKING_DIR) && rm -f $(ABS_BUILD_PATH)/$(APP_NAME)_$(VERSION_STRING).debian.tar.gz
 	# The dpkg-buildpackage call construct a "debian" and "orig" tarballs,
 	# then extracts the tarball into the build directory. Then command then
 	# operates on that copy of the source code: itself running a make
@@ -67,7 +56,7 @@ deb:
 	# deb file.
 	# Taking care to pass in environment variable for the Makefile invocation
 	$(info * Make deb package by, among other things, running make on a copy of this very Makefile)
-	cd $(PACKING_DIR) && APP_NAME=$(APP_NAME) dpkg-buildpackage -b -us -uc
+	cd $(PACKING_DIR) && APP_NAME=$(APP_NAME) dpkg-buildpackage -us -uc
 
 clean: 
 	rm -rf $(ABS_BUILD_PATH)
